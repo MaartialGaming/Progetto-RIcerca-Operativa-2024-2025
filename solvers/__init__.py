e9641eac117203039fbeee6b9449c700953292e7
rezathriii <taheri.reza94@gmail.com>
2025-05-26 14:19:11 +0200

    first init, setup project like what it is explained in project description

--- solvers/__init__.py
+++ solvers/__init__.py
@@ -1,5 +1,7 @@
 from .dummy_solver import DummySolver
+from .solver_327795_327796 import solver_327795_327796
 
 __all__ = [
-    'DummySolver'
+    'DummySolver',
+    'solver_327795_327796'
 ]
--- solvers/abstract_solver.py
+++ solvers/abstract_solver.py
@@ -3,13 +3,13 @@ from abc import ABC, abstractmethod
 from environments import Environment
 import numpy as np
 
+
 class AbstractSolver(ABC):
     @abstractmethod
     def __init__(self, env: Environment):
         self.name = None
         self.env = env
 
-    
     @abstractmethod
     def solve(self):
         pass
